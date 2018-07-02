437132f6600317898eff54ad30ca3b47f1d59e51
olibraun <oliverbraun89@gmail.com>
2018-07-02 10:27:18 +0200

    fix: Anpassung Testmail-Skript wg. Encoding

--- testmail.sh
+++ testmail.sh
@@ -3,5 +3,5 @@
 source /var/services/homes/oliver/Scripts/picdump.conf
 
 subject="Testmail des Picdump-Notifiers"
-msg="Dies ist eine Testmail vom Picdump-Notifier, mit der der Mailversand und der Empfängerkreis getestet wird."
+msg="Dies ist eine Testmail vom Picdump-Notifier, mit der der Mailversand getestet wird. Wenn Sie diese Mail lesen, werden Sie wahrscheinlich auch zum Erscheinen des Picdumps informiert werden."
 /usr/bin/php -r "mail('$toaddress','$subject','$msg','$fromaddress');"
