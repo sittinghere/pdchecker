308d31d3a2af95ab3623f46d51995dfad8f94ba3
OB <oliverbraun89@gmail.com>
2018-07-15 10:34:16 +0200

    feat: Update/adjust test script as well

--- pd_testmail.sh
+++ pd_testmail.sh
@@ -7,7 +7,15 @@ sendmail() {
   /usr/bin/php -r "mail('$1','$2','$3','$4');"
 }
 
+sendmaillist() {
+  # Send mails to each of the recipients in the list specified in the conf file
+  # Arguments: subject, message, sender address
+  for address in "${arrayName[@]}"; do
+    sendmail $address $1 $2 $3
+  done
+}
+
 subject="Testmail des Picdump-Notifiers"
 msg="Dies ist eine Testmail vom Picdump-Notifier, mit der der Mailversand getestet wird. Wenn Sie diese Mail lesen, werden Sie wahrscheinlich auch zum Erscheinen des Picdumps informiert werden."
 
-sendmail "$toaddress" "$subject" "$msg" "$fromaddress"
+sendmaillist "$subject" "$msg" "$fromaddress"
