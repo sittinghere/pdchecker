8b5f55c20321be5f5e7ab03e66ba8a117012fff8
OB <oliverbraun89@gmail.com>
2018-07-15 10:08:18 +0200

    feat: Call logger in script

--- pd_checker.sh
+++ pd_checker.sh
@@ -3,10 +3,10 @@
 workingdirectory="/var/services/homes/oliver/Scripts/"
 
 # Get the mail recipients for the picdump notifier
-source "$workingdirectory"picdump.conf
+source "$workingdirectory""/picdump.conf"
 
 # Create log file if it doesn't exist yet
-logfile="$workingdirectory""pdchecker.log";
+logfile="$workingdirectory""/pd_checker.log";
 touch $logfile
 
 # ###############################################################################
@@ -36,6 +36,8 @@ logger(){
 # Beginning of the script
 # ###############################################################################
 
+logger "Start"
+
 # Get today's date
 heute=$(date +%d.%m.%Y)
 
@@ -43,7 +45,9 @@ done=0
 
 while [ $done = 0 ]; do
   # Get date and URL of latest picdump
+  logger "Checking bildschirmarbeiter.com..."
   page=$(curl --silent http://www.bildschirmarbeiter.com/plugs/category/picdumps/)
+  logger "Check completed."
   searchstring="<p>Bildschirmarbeiter - Picdump vom "
   index=$(strindex "$page" "$searchstring")
   letzter_dump=${page:$index+36:10}
@@ -52,15 +56,19 @@ while [ $done = 0 ]; do
   # Check if the latest picdump is from today
   if [[ "$letzter_dump" = "$heute" ]]; then
     # There is a new picdump, send mail notification
+    logger "Picdump is available."
     subject="Picdump"
     msg="The latest picdump is online at $url_letzter_dump"
+    logger "Sending mail to $toaddress"
     sendmail "$toaddress" "$subject" "$msg" "$fromaddress"
     done=1
   else
+    logger "Picdump not available yet, re-checking in 30 seconds."
     # What time is it?
     hour=$(date +%H)
     if [ "$hour" -ge "18" ]; then
       # We stop this script at 18:00 at the latest and send out a mail that there was no picdump today.
+      logger "Cutoff time reached, no picdump available today."
       subject="No Picdump"
       msg="It is now after 18:00 and the picdump notifier is shutting down. It seems there was no new picdump today."
       sendmail "$toaddress" "$subject" "$msg" "$fromaddress"
@@ -71,3 +79,13 @@ while [ $done = 0 ]; do
     fi
   fi
 done
+      sendmail "$toaddress" "$subject" "$msg" "$fromaddress"
+      done=1
+    else
+      # The new picdump is not available yet, re-check in 30 seconds.
+      sleep 30
+    fi
+  fi
+done
+
+logger "End"
